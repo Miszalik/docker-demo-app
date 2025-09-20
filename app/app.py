128352bd820d343acc8f21b52b53590ab62622c0
Mateusz Michalik <63094399+Miszalik@users.noreply.github.com>
2025-09-21 01:42:55 +0200

    chore: dockeryzing

--- app/app.py
+++ app/app.py
@@ -15,4 +15,4 @@ def index():
     return render_template('index.html', visits=visits)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
